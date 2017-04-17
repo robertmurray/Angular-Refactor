82632e07e0fec54883eabc2bc85cb38565757a02
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-04-17 16:16:16 -0700

    Added search button and search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,6 @@ import { HeaderComponent } from './header/header.component';
 import { CheeseComponent } from './cheese/cheese.component';
 import { SearchComponent } from './cheese/search/search.component';
 import { DisplayComponent } from './cheese/display/display.component';
-import { SearchBarComponent } from './cheese/search-bar/search-bar.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { DisplayFavoritesComponent } from './favorites/display-favorites/display-favorites.component';
 import { NoteTextAreaComponent } from './favorites/note-text-area/note-text-area.component';
@@ -23,7 +23,6 @@ import { NoteTextAreaComponent } from './favorites/note-text-area/note-text-area
     CheeseComponent,
     SearchComponent,
     DisplayComponent,
-    SearchBarComponent,
     FavoritesComponent,
     DisplayFavoritesComponent,
     NoteTextAreaComponent,
@@ -32,7 +31,8 @@ import { NoteTextAreaComponent } from './favorites/note-text-area/note-text-area
     BrowserModule,
     FormsModule,
     HttpModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    ButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
